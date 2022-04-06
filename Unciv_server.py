e72bc2d5650f461107b3a2de9ba1853fbb197f86
Mape6 <77325495+Mape6@users.noreply.github.com>
2022-04-06 14:49:34 +0200

    Updated comments Changed order of checks in GET and PUT methods Added TODO for DELETE method

--- Unciv_server.py
+++ Unciv_server.py
@@ -32,35 +32,32 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
-        # Response for connection check
-        if self.path.endswith('isalive'):
+        # Check for preview file name and send content to client
+        if re.search(f'\/files\/{uuid_regex}_Preview$', self.path):
+            path = self.translate_path(self.path)
+            self.send_file_content(path)
+        # Check for game file name and send content to client
+        elif re.search(f'\/files\/{uuid_regex}$', self.path):
+            path = self.translate_path(self.path)
+            self.send_file_content(path)
+        # Check for connection check and response with 'true'
+        elif self.path.endswith('isalive'):
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write("true".encode())
-        # Response with game preview file
-        elif re.search(f'\/files\/{uuid_regex}_Preview$', self.path):
-            path = self.translate_path(self.path)
-            self.send_file_content(path)
-        # Response with game file
-        elif re.search(f'\/files\/{uuid_regex}$', self.path):
-            path = self.translate_path(self.path)
-            self.send_file_content(path)
+        # Everything else is not allowed
         else:
             self.send_response(401)
             self.end_headers()
 
     def do_PUT(self):
-        # Check if path ends with '/' that is not allowed
-        if self.path.endswith('/'):
-            self.send_response(405)
-            self.end_headers()
-        # Check for preview file name
-        elif re.search(f'\/files\/{uuid_regex}_Preview$', self.path):
+        # Check for preview file name and write content to file
+        if re.search(f'\/files\/{uuid_regex}_Preview$', self.path):
             path = self.translate_path(self.path)
             content_length = int(self.headers['Content-Length'])
             self.write_file_content(path, content_length)
-        # Check for game file name
+        # Check for game file name and write content to file
         elif re.search(f'\/files\/{uuid_regex}$', self.path):
             path = self.translate_path(self.path)
             content_length = int(self.headers['Content-Length'])
@@ -69,6 +66,9 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
         else:
             self.send_response(401)
             self.end_headers()
+    
+    def do_DELETE(self):
+        # TODO: write method for deleting a game file
 
 
 Handler = MyHttpRequestHandler
